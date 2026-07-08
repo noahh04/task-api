6442d0d0ccba47b90de9100752e42a2771d41474
Noah Wehbe <noahwehbe1@gmail.com>
2026-07-08 11:50:12 +0200

    Added input validation for hero schemas

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 from sqlmodel import SQLModel, Field
 
 class HeroBase(SQLModel):
-    name: str = Field(index=True)
-    age: int | None = Field(default=None, index=True)
+    name: str = Field(min_length=2, index=True)
+    age: int | None = Field(default=None, ge=0, le=150, index=True)
 
 class Hero(HeroBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Field
 from models import HeroBase
 
 
@@ -8,10 +8,10 @@ class HeroPublic(HeroBase):
 
 
 class HeroCreate(HeroBase):
-    secret_name: str
+    secret_name: str = Field(min_length=2)
 
 
-class HeroUpdate(HeroBase):
-    name: str | None = None
-    age: int | None = None
-    secret_name: str | None = None
+class HeroUpdate(SQLModel):
+    name: str | None = Field(default = None, min_length=2)
+    age: int | None = Field(default = None, ge=0, le=150)
+    secret_name: str | None = Field(default = None, min_length=2)
